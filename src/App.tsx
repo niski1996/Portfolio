62560d9d26195eac6614951785745d241e9ef6f1
niski1996 <niski1996@interia.pl>
2025-04-08 23:49:37 +0200

    Autogenerated commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,25 @@
+import './i18n';
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Comp1 from './components/Comp1';
+import Comp2 from './components/Comp2';
+import Comp3 from './components/Comp3';
+
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+    <div>dupa
+    <Comp1 />
+      <hr />
+      <Comp2 />
+      <hr />
+      <Comp3 />
+    </div>
       <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
