b8adc289b078218629057f2e08a02a14e6724ded
niski1996 <niski1996@interia.pl>
2025-04-12 23:00:43 +0200

    Autogenerated commit

--- src/components/ProjectTile.tsx
+++ src/components/ProjectTile.tsx
@@ -1,13 +1,44 @@
 import { useTranslation } from "react-i18next";
+import PointingArrow from "./PointingArrow";
 
 function ProjectTile() {
-    const { t } = useTranslation();
-    return (
-        <div className="group relative grid gap-4 pb-1 transition-all sm:grid-cols-8 sm:gap-8 md:gap-4 lg:hover:!opacity-100 lg:group-hover/list:opacity-50">
-dupa
-        </div>
-    
-    )
-
+  const { t } = useTranslation();
+  
+  return (
+    <article className="group relative grid gap-4 sm:grid-cols-8 sm:gap-8 transition-all mt-8 mb-8">
+      <div className="absolute -inset-4 z-0 hidden rounded-md lg:block lg:group-hover:bg-slate-800/50" />
+      
+      <div className="z-10 sm:col-span-6 sm:order-2">
+        <h3>
+          <a 
+            href="https://www.newline.co/courses/build-a-spotify-connected-app" 
+            target="_blank"
+            rel="noreferrer"
+            className="inline-flex items-baseline font-medium text-slate-200 hover:text-teal-300 group/link"
+          >
+            Build a Spotify Connected{" "}
+            <span className="inline-flex items-center">
+              App
+              <PointingArrow />
+            </span>
+          </a>
+        </h3>
+        <p className="mt-2 text-sm">
+          {"ssp"} - A Spotify connected app that allows users to create and share playlists with friends. Built using React, Node.js, and the Spotify API.'
+        </p>
+      </div>
+      
+      {/* Image */}
+      <img 
+        alt={"ss"}
+        loading="lazy"
+        width={200}
+        height={48}
+        className="aspect-video rounded border-2 border-slate-200/10 sm:order-1 sm:col-span-2"
+        src="https://picsum.photos/200/48" 
+      />
+    </article>
+  );
 }
+
 export default ProjectTile;
